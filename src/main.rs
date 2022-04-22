0668eac0c5c1420e862e3243d922e5ec4bc7334e
Enn3Developer <arturpeshko39@gmail.com>
2022-04-22 22:39:48 +0200

    Work on the homepage

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,35 @@ async fn index() -> impl Responder {
         .body(include_str!("../website/home.html"))
 }
 
-#[get("/images/{image}")]
-async fn get_image(image: Path<String>) -> impl Responder {
-    let bytes =
-        Bytes::from(fs::read_to_string(format!("website/images/{}", image.into_inner())).unwrap());
+#[get("/images/{image}.jpg")]
+async fn get_image_jpg(image: Path<String>) -> impl Responder {
+    let bytes = Bytes::from(
+        fs::read_to_string(format!("website/images/{}.jpg", image.into_inner())).unwrap(),
+    );
     HttpResponse::build(StatusCode::OK)
         .content_type("image/jpeg")
         .body(bytes)
 }
 
+#[get("/images/{image}.svg")]
+async fn get_image_svg(image: Path<String>) -> impl Responder {
+    let bytes = Bytes::from(
+        fs::read_to_string(format!("website/images/{}.svg", image.into_inner())).unwrap(),
+    );
+    HttpResponse::build(StatusCode::OK)
+        .content_type("image/svg+xml")
+        .body(bytes)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(get_image_jpg)
+            .service(get_image_svg)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
